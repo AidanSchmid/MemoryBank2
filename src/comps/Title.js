9befd52865cb2ee73ef2b00f3c3a3245be8ad144
AidanSchmid <aschmid@quinnipiac.com>
2022-12-10 01:01:07 -0500

    Update

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -4,7 +4,7 @@ const Title = () => {
   return (
     <div className="title">
       <h1>Memory Bank</h1>
-      <h2>Mary and David</h2>
+      <h2>Jack and Doriana</h2>
       <p>Upload Your Memories Here!</p>
     </div>
   );
